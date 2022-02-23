aa2b31672fec9741c0b089daa619e4327c12c23f
Dodsie <baileydods@gmail.com>
2022-02-22 21:56:33 -0700

    javascripting final files

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math(x,y,z){
+  let multiply = y * z;
+  return multiply + x;
+}; console.log(math(53,61,67));
---
+++ object-keys.js
@@ -0,0 +1,6 @@
+const car = {
+  make: 'Toyota',
+  model:'Accord',
+  year:2020
+}; const keys = Object.keys(car);
+console.log(keys);
